2f379508555cb599346d59450a63779f2dd13765
thegreatLSJ <junee526@gmail.com>
2017-12-05 03:49:15 +0900

    following jest with typescript example

--- src/tests/CooniBtn.test.tsx
+++ src/tests/CooniBtn.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import TestUtils from 'react-dom/test-utils';
 
 import CooniBtn from '@shared/CooniBtn';
 
-test('CooniBtn changes border color when it is clicked', () => {
-  const CooniBtn: any = renderer.create(
+it('CooniBtn changes border color when it is clicked', () => {
+  const CooniBtn: any = TestUtils.renderIntoDocument(
     <CooniBtn />
   )
 });
