873ff2c14a9b5ea369663ec5433b8e4dbd1b4fc7
thegreatlsj <junee526@gmail.com>
2017-12-18 20:11:44 +0900

    removing js files in dist directory

--- dist/apis/authApi.js
+++
@@ -1,4 +0,0 @@
-const LOGIN_USER_API = '/api/auth/login';
-const SIGNUP_USER_API = '/api/auth/signup';
-const IS_LOGGED_API = '/api/auth/isAuthenticated';
-//# sourceMappingURL=authApi.js.map
--- dist/apis/memberApi.js
+++
@@ -1,4 +0,0 @@
-const GET_MEMBERS_URL = '/api/user/all';
-const GET_MEMBER_URL = '/api/user/one/';
-const UPDATE_MEMBER_URL = '/api/user/update/';
-//# sourceMappingURL=memberApi.js.map
