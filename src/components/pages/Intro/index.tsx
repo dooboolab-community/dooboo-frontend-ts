26602f75c6583c9a5ec10a56b8a49a19a9d041f5
hyochan <dooboolab@gmail.com>
2018-01-07 14:15:31 +0900

    Fixed and implement Intro page.
    Get rid of raidum for Intro page and designed for modular postcss style instead.

--- src/components/pages/Intro/index.tsx
+++ src/components/pages/Intro/index.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
-import Radium from 'radium';
 
 import { colors, effects } from '@utils/styles';
 import CooniBtn from '@shared/CooniBtn';
@@ -10,9 +9,10 @@ import {
   facebook_logo_1x, facebook_logo_2x, facebook_logo_3x,
 } from '@utils/Icons';
 
+const styles = require('./style.css');
+
 @inject('store')
 @observer
-@Radium
 class Intro extends Component<any> {
   public login(oauth: string) {
     this.props.store.user.login(`user from ${oauth}`);
@@ -29,9 +29,9 @@ class Intro extends Component<any> {
         {
           this.props.store.user.loggedIn
           ? <Redirect to='/tab/tab1' />
-          : <div className='gradientContainer' style={styles.container}>
-              <div style={styles.introContainer}>
-                <div style={styles.oauthBox}>
+          : <div className={styles.container}>
+              <div className={styles.introContainer}>
+                <div className={styles.oauthBox}>
                     <CooniBtn
                       onClick={() => this.goToSignUp()}
                       white={true}
@@ -59,77 +59,5 @@ class Intro extends Component<any> {
     );
   }
 }
-
-const styles: any = {
-  container: {
-    // display: 'flex',
-    // background: colors.cooniGradient,
-    // above properties are in defined in css file out of the src/ directory to show users that postcss works
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  introContainer: {
-    margin: '28px',
-    width: 'calc(100%-56px)',
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    position: 'relative',
-  },
-
-  oauthBox: {
-    bottom: 0,
-    position: 'absolute',
-  },
-
-  oauthBtn: {
-    color: '#fff',
-    width: '320px',
-    height: '28px',
-    borderRadius: '3px',
-    border: 'solid 2px #fff',
-    margin: '8px 0',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-
-    ...effects.btnEffect,
-
-    ':hover': {
-      ...effects.btnEffectHover,
-    },
-    ':active': {
-      opacity: 0.5,
-    },
-
-    '@media (max-width: 740px)': {
-      width: '85vw',
-      height: '8vh',
-    },
-  },
-
-  googleLogoImg: {
-    position: 'absolute',
-    left: '16px',
-    height: '20px',
-    width: '20px',
-    objectFit: 'cover',
-  },
-
-  fbLogoImg: {
-    position: 'absolute',
-    left: '16px',
-    height: '20px',
-    width: '20px',
-    objectFit: 'cover',
-  },
-};
 
 export default Intro;
