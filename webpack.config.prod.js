1ba1f58bb5e8333465d56ff93c6913c83f6275aa
hyochan <dooboolab@gmail.com>
2018-02-05 01:01:27 +0900

    Setup webpack for prod.

---
+++ webpack.config.prod.js
@@ -0,0 +1,84 @@
+const webpack = require('webpack');
+const path = require('path');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
+  resolve: {
+    modules: [
+      './node_modules',
+      './functions',
+    ],
+    extensions: ['.ts', '.tsx', '.js', 'jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: { presets: ['react', 'env', 'stage-0'] },
+          },
+          {
+            loader: 'ts-loader',
+          },
+        ],
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.js(x?)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: { presets: ['react', 'env', 'stage-0'] },
+          },
+        ],
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.css$/,
+        use: [ 
+          'style-loader',
+          { 
+            loader: 'css-loader',
+            options: { 
+              importLoaders: 1,
+              // modules: true,
+              // you can remove the comment in above code if you wanna uglify css classnames to scope to specific component
+            },
+          },
+          { 
+            loader: 'postcss-loader', 
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('postcss-import')(),
+                require('postcss-cssnext')(),
+              ]
+            },
+          },
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      }
+    ]
+  },
+};
