5baba122945ed130f4d43c1852dadae422ff01ed
hyochan <dooboolab@gmail.com>
2019-10-08 18:10:23 +0900

    Update packages and webpack setup
    - Remove ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,9 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/',
   },
-  plugins: [
-  ],
+  plugins: [],
   resolve: {
-    modules: [
-      './node_modules',
-    ],
+    modules: ['./node_modules'],
     extensions: ['.ts', '.tsx', '.js', 'jsx'],
   },
   devServer: {
@@ -28,56 +25,19 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts(x?)$/,
+        test: /\.(js|jsx|tsx|ts)$/,
         use: [
           {
             loader: 'babel-loader',
             query: {
               cacheDirectory: true,
-              presets: [
-                '@babel/preset-env',
-                '@babel/preset-react',
-              ],
-              plugins: [
-                'react-hot-loader/babel',
-                '@babel/plugin-proposal-function-sent',
-                '@babel/plugin-proposal-export-namespace-from',
-                '@babel/plugin-proposal-numeric-separator',
-                '@babel/plugin-proposal-throw-expressions',
-                ['@babel/plugin-proposal-class-properties', { 'loose': true }],
-                [
-                  '@babel/plugin-transform-runtime',
-                  {
-                    'helpers': true,
-                    'regenerator': false,
-                  },
-                ],
-              ],
-            },
-          },
-          {
-            loader: 'ts-loader',
-          },
-        ],
-        exclude: [/node_modules/],
-      },
-      {
-        test: /\.js(x?)$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            query: {
-              cacheDirectory: true,
-              presets: [
-                '@babel/preset-env',
-                '@babel/preset-react',
-              ],
+              presets: ['@babel/preset-env', '@babel/preset-react'],
               plugins: [
                 [
                   '@babel/plugin-transform-runtime',
                   {
-                    'helpers': true,
-                    'regenerator': false,
+                    helpers: true,
+                    regenerator: false,
                   },
                 ],
               ],
@@ -96,6 +56,9 @@ module.exports = {
         ],
       },
     ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
   },
   devtool: 'inline-source-map',
 };
