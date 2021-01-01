ea6deaad512ac0af2a38a2710d5fb21613a2f0a3
hyochan <dooboolab@gmail.com>
2021-01-02 02:07:14 +0900

    Removed dup lint config in @dooboo/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,6 @@ module.exports = {
   root: true,
   extends: '@dooboo/eslint-config',
   rules: {
-    'react/display-name': 0,
-    '@typescript-eslint/no-var-requires': 0,
-    'react-native/no-inline-styles': 0,
     'eslint-comments/no-unlimited-disable': 0,
     'eslint-comments/no-unused-disable': 0,
   },
