edab64be2336517cd01d27164bf83e3610508c30
hyochan <dooboolab@gmail.com>
2022-07-01 20:28:55 +0900

    Update pkgs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['@dooboo/eslint-config', 'plugin:jest/recommended'],
+  extends: ['@dooboo/eslint-config'],
   rules: {
     'eslint-comments/no-unlimited-disable': 0,
     'eslint-comments/no-unused-disable': 0,
