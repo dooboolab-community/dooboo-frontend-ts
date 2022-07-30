52457787ba4dae005f671c738094c38a3ed10912
hyochan <dooboolab@gmail.com>
2022-07-30 21:36:15 +0900

    Update eslint config and yarn pkg version

--- test/testUtils.tsx
+++ test/testUtils.tsx
@@ -24,3 +24,13 @@ export const createTestElement = (
     </BrowserRouter>
   </RootProvider>
 );
+
+export const createTestProps = (
+  obj?: Record<string, unknown>,
+): Record<string, unknown> | unknown | any => ({
+  navigation: {
+    navigate: jest.fn(),
+    goBack: jest.fn(),
+  },
+  ...obj,
+});
