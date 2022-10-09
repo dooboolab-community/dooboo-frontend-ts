b706bb92fb2431730ee6232732ea3f0ddb1665c0
hyochan <dooboolab@gmail.com>
2022-10-09 16:09:31 +0900

    feat: toggle theme type with `ctrl + .`

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
 import {createRoot, hydrateRoot} from 'react-dom/client';
 
-import {ReactElement} from 'react';
+import type {ReactElement} from 'react';
 import RootProvider from './providers';
 import SwitchNavigator from './components/navigations/SwitchNavigator';
 import {initFbt} from './utils/fbt';
+import {useEffect} from 'react';
+import {useThemeContext} from './providers/ThemeProvider';
 
 initFbt();
 
 const rootElement = document.getElementById('app') as HTMLElement;
 const root = createRoot(rootElement);
 
-const Component = (): ReactElement => (
-  <RootProvider>
-    <SwitchNavigator />
-  </RootProvider>
-);
+function App(): ReactElement {
+  const {changeThemeType} = useThemeContext();
+
+  useEffect(() => {
+    (() => {
+      document.addEventListener('keydown', (event) => {
+        if (event.ctrlKey && event.key === '.') {
+          changeThemeType();
+        }
+      });
+    })();
+
+    return () => {
+      document.removeEventListener('keydown', () => {});
+    };
+  }, [changeThemeType]);
+
+  return <SwitchNavigator />;
+}
+
+function Root(): ReactElement {
+  return (
+    <RootProvider
+      initialThemeType={
+        window?.matchMedia('(prefers-color-scheme: dark)').matches
+          ? 'dark'
+          : 'light'
+      }
+    >
+      <App />
+    </RootProvider>
+  );
+}
 
 const renderApp = (): void => {
   if (rootElement.hasChildNodes()) {
-    hydrateRoot(rootElement, <Component />);
+    hydrateRoot(rootElement, <Root />);
   } else {
-    root.render(<Component />);
+    root.render(<Root />);
   }
 };
 
