41b96e8a3a793f4920b9f07213eb98cf6e2df467
DevYuns <ydhun93@gmail.com>
2023-01-31 17:49:27 +0900

    fix: modify a test case to be more accurately

--- test/components/navigations/SwitchNavigator.test.tsx
+++ test/components/navigations/SwitchNavigator.test.tsx
@@ -1,11 +1,29 @@
+import type * as ThemeProviderType from '../../../src/providers/ThemeProvider';
+
+import {expect, test, vi} from 'vitest';
+
 import React from 'react';
 import RootProvider from '../../../src/providers';
 import SwitchNavigator from '../../../src/components/navigations/SwitchNavigator';
 import {render} from '@testing-library/react';
-import {test} from 'vitest';
 import userEvent from '@testing-library/user-event';
 
 const props = {};
+
+const mockFn = vi.fn();
+
+vi.mock('../../../src/providers/ThemeProvider', async () => {
+  const lib = (await vi.importActual(
+    '../../../src/providers/ThemeProvider',
+  )) as typeof ThemeProviderType;
+
+  return {
+    ...lib,
+    useThemeContext: () => ({
+      changeThemeType: mockFn,
+    }),
+  };
+});
 
 test('renders without crashing', () => {
   const testingLib = render(<SwitchNavigator {...props} />, {
@@ -13,19 +31,23 @@ test('renders without crashing', () => {
       <RootProvider initialThemeType="light">{children}</RootProvider>
     ),
   });
+  expect(testingLib).toBeTruthy();
 
   testingLib.unmount();
 });
 
 test('should switch theme type with keyboard', async () => {
   const user = userEvent.setup();
+
   const testingLib = render(<SwitchNavigator {...props} />, {
     wrapper: ({children}) => (
       <RootProvider initialThemeType="light">{children}</RootProvider>
     ),
   });
 
-  await user.keyboard('{ControlLeft>}./');
+  await user.keyboard('[ControlLeft>].');
+
+  expect(mockFn).toBeCalled();
 
   testingLib.unmount();
 });
